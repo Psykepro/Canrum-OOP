a301361c78b6592bae115fcb6acf8024215b18b0
Metodi Krustev <metodi.krustev87@gmail.com>
2015-06-23 17:49:32 +0300

    added Wizardrelic

---
+++ CanrumRPG/CanrumRPG/Items/Equipable/WizardRelic.cs
@@ -0,0 +1,12 @@
+﻿namespace CanrumRPG.Items
+{
+    using Engine;
+
+    public class WizardRelic : Equipable
+    {
+        public WizardRelic(Position position)
+                    : base(position, "WizardRelic", 30, 0, 0, 0)
+        {
+        }
+    }
+}
