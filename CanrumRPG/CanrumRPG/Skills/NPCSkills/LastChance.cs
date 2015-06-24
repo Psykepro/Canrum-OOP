d810c5ae1a3b75b0b5c98dca284599b6398f2f28
Psykepro <nickolay_nedkov@abv.bg>
2015-06-24 11:54:16 +0300

    NPCs skill Heal Spirit was changed with Last Chance

---
+++ CanrumRPG/CanrumRPG/Skills/NPCSkills/LastChance.cs
@@ -0,0 +1,28 @@
+﻿namespace CanrumRPG.Skills.NPCSkills
+{
+    using Characters;
+
+    using Enums;
+
+    using System.Timers;
+
+
+
+    public class LastChance : PassiveSkill
+    {
+        public LastChance(CharClass charClass) : base(0, 15, 0, 0, charClass)
+        {
+        }
+
+        public override void ApplySkillStats(Character caster)
+        {
+            int under30 = (caster.CurrentHealth*30)/100;
+            if (caster.CurrentHealth<under30)
+            {
+                caster.BlockChance += this.DefenseModifier;
+                caster.DefenseRating += this.DefenseModifier/2;
+            }
+        }
+
+    }
+}
